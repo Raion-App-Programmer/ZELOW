5199faff33b95d58000840ae1d06ba95d73546e8
rhesatsaqif23 <atstsaqif23@gmail.com>
2025-06-02 11:52:47 +0700

    Resolve merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         '/profile': (context) => ProfilePage(),
         '/pesanan': (context) => PesananPage(orders: [],),
         '/profile': (context)=> ProfilePage(),
-        '/toko': (context)=> TokoPageUser()
+        '/toko': (context)=> TokoPageUser(),
         '/chat': (context)=> chatPage(),
       },
     );
