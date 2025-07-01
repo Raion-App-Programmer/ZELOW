3b5d54d13415e367e096f929144a5c55a3f7bcb0
femnix <femnitality@gmail.com>
2025-07-01 14:51:20 +0700

    Made a dummy stock, finished with everything.

--- lib/pages/umkm/stok_toko_umkm.dart
+++ lib/pages/umkm/stok_toko_umkm.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:zelow/components/constant.dart';
@@ -28,6 +30,7 @@ class StokTokoUmkm extends StatelessWidget {
           Expanded(
             child: ListView(
               children: [
+                DummyStok()
               ],
             ),
           ),
@@ -71,6 +74,187 @@ class TambahProduk extends StatelessWidget {
           )
         )
       ],
+    );
+  }
+}
+
+class DummyStok extends StatefulWidget {
+  const DummyStok({super.key});
+
+  @override
+  State<DummyStok> createState() => _DummyStokState();
+}
+
+class _DummyStokState extends State<DummyStok> {
+  int _currentStock = 0;
+  bool _isAvailable = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentStock = 0;
+  }
+
+  void increaseStock() {
+    setState(() {
+      _currentStock++;
+    });
+  }
+  void decreaseStock() {
+   if (_currentStock > 0) {
+    setState(() {
+      _currentStock--;
+    });
+   }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 14, right: 20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffFEFEFE),
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: Color(0xffE6E6E6),
+            width: 2,
+            ),
+        ),
+        child: 
+        Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.blueGrey
+                    ),
+                    width: 94.30,
+                    height: 90,
+                  ),
+                ),
+                SizedBox(width: 16,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                    'Nasi Padang saus tiram',
+                    style: blackTextStyle.copyWith(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.start,
+                    ),
+                    Text(
+                      'RP15.000',
+                      style: greyTextStyle.copyWith(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(
+                          '$_currentStock | Tersedia',
+                          style: greenTextStyle.copyWith(
+                            wordSpacing: 4,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        // increment and decrement button
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Color(0xff06C474)),
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(0.0),
+                            child: Row(
+                              children: [
+                                // decrement button
+                                GestureDetector(
+                                  onTap: decreaseStock,
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                      child: Icon(
+                                        Icons.remove,
+                                        size: 20,
+                                        color: _currentStock > 0 ? Color(0xff06C474) : Color(0xffE6E6E6),
+                                      ),
+                                    ),
+                                  ),
+                                // current stock
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 8),
+                                  child: Text(
+                                    '$_currentStock',
+                                    style: greenTextStyle.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                                // increment button
+                                GestureDetector(
+                                  onTap: increaseStock,
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                  child: Icon(
+                                    Icons.add,
+                                    size: 20,
+                                    color: Color(0xff06C474),
+                                  ),
+                                ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 2,),
+            Row(
+              children: [
+                Switch(
+                  value: _isAvailable,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _isAvailable = value;
+                    });
+                  },
+                  activeColor: Colors.green,
+                  thumbColor: WidgetStateProperty.resolveWith<Color?>(
+                    (states) {
+                      if (states.contains(WidgetState.selected)) {
+                        return Colors.white; 
+                      }
+                      return Colors.white;
+                    },
+                  ),
+            ),
+            Text(
+              'Tampilkan',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            )
+          ],
+        ),
+          ],
+      ),
+      ),
     );
   }
 }
